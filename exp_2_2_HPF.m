d6f48c20cfa81f4173b7d6b77380ae5366efe5fd
asifrahman15 <asifrahman15@gmail.com>
2021-08-24 12:56:00 +0530

    Exp - 2.2 : High Pass Filter

---
+++ exp_2_2_HPF.m
@@ -0,0 +1,18 @@
+// Exp - 2.2 : High Pass Filter
+clc;
+f=0:100:5000;
+fc=2000;
+rk=600;
+l=rk/(4*pi*fc);
+c=1/(4*pi*rk*fc);
+a=2*acosh(fc./f);
+b=2*asin(fc./f);
+plot(f,a,'*-r',f,b,'+-b');
+xlabel('Frequency');
+ylabel('Attenuation and Phase constant');
+title('CONSTANT K HIGH PASS FILTER');
+legend('Attenuation', 'Phase constant');
+disp('Inductance : ')
+l
+disp('Capacitance : ')
+c
